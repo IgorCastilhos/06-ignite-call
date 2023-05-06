f974d82eb0a8b443aabbc60c3954c3afe160df85
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-05-05 23:51:25 -0300

    Add: Creating user register page

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,63 @@
+import { TextInput } from '@ignite-ui/react'
+import { Button, Text } from '@igor-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
+import { useRouter } from 'next/router'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(8, { message: 'O usuário precisa ter pelo menos 8 letras.' })
+    .max(30, { message: 'O usuário pode ter no máximo 30 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type claimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<claimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  const router = useRouter()
+
+  async function handleClaimUsername(data: claimUsernameFormData) {
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          prefix="site.com/"
+          placeholder="seu-usuário"
+          size="sm"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit" disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size={'sm'}>
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
