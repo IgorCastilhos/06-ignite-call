8d6352b511a48cd8d32e814ae5f49d01f5ab38d3
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-05-23 00:46:57 -0300

    Finalizando queries e bloqueando horários

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -117,7 +117,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
